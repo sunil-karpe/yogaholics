41e8eb81cd809f8b9990bef552a3edf53760a390
Admin <karpe4858@gmail.com>
2021-07-23 13:18:40 +0530

    Sling model for multicard

---
+++ yogaholics/core/src/main/java/yogaholics/core/models/Multicard.java
@@ -0,0 +1,24 @@
+package yogaholics.core.models;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+ 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional; 
+import org.apache.sling.settings.SlingSettingsService;
+ 
+@Model(adaptables = Resource.class)
+public class Multicard {
+ 
+ // Inject the products node under the current node
+ @Inject
+ @Optional
+ public Resource cards;
+ 
+ // No need of a post construct as we don't have anything to modify after the
+ // model is constructed
+}
+
